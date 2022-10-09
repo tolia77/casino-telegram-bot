12b75cfb11da0a87a4e95417ca4414f00fc332d2
Anatoliy Kokhan <85370550+tolia77@users.noreply.github.com>
2022-10-09 13:07:49 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 from db import Database
-bot_token = "5636814074:AAETlsUroMjtMkQinVDKtPMYzlBaTrQ85Pk"
+bot_token = "token"
 host = "127.0.0.1"
 db_name = "casino_telegram_bot"
 user = "postgres"
-password = "postpass"
+password = "password"
 
 client = Database(host=host, database=db_name, user=user, password=password)
 users_list = client.get_users()
